aecc89f70501ad7b66b463373666aad2d386d9e2
Dennis Gwafan <denz.aviator@gmail.com>
2023-12-19 14:36:44 +0100

    file updated

--- src/DiamondPricePrediction/components/data_ingestion.py
+++ src/DiamondPricePrediction/components/data_ingestion.py
@@ -11,21 +11,19 @@ from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 from pathlib import Path
 
-
 class DataIngestionConfig:
     raw_data_path:str=os.path.join("artifacts","raw.csv")
     train_data_path:str=os.path.join("artifacts","train.csv")
     test_data_path:str=os.path.join("artifacts","test.csv")
 
+
 class DataIngestion:
     def __init__(self):
         self.ingestion_config=DataIngestionConfig()
         
-
     def initiate_data_ingestion(self):
         logging.info("data ingestion started")
                 
-        
             
         try:
             data=pd.read_csv(Path(os.path.join("notebooks/data","diamond.csv.CSV")))
