eb16b9b35e64f0e4c47cd9a19b154d4f41a02a89
Dennis Gwafan <denz.aviator@gmail.com>
2023-12-22 20:59:16 +0100

    file update

--- src/DiamondPricePrediction/components/model_evaluation.py
+++ src/DiamondPricePrediction/components/model_evaluation.py
@@ -7,8 +7,7 @@ import mlflow
 import mlflow.sklearn
 import numpy as np
 import pickle
-from src.DimondPricePrediction.utils.utils import load_object
-
+from src.DiamondPricePrediction.utils import load_object
 
 
 class ModelEvaluation:
@@ -16,7 +15,7 @@ class ModelEvaluation:
         pass
     
     
-    def eval_metrics(self,actual, pred):
+    def eval_metrics(self,actual,pred):
         rmse = np.sqrt(mean_squared_error(actual, pred))# here is RMSE
         mae = mean_absolute_error(actual, pred)# here is MAE
         r2 = r2_score(actual, pred)# here is r3 value
@@ -24,7 +23,7 @@ class ModelEvaluation:
     
     def initiate_model_evaluation(self,train_array,test_array):
         try:
-            X_test,y_test=(test_array[:,:-1], test_array[:,-1])
+            X_test,y_test=(test_array[:,:-1],test_array[:,-1])
 
             model_path=os.path.join("artifacts","model.pkl")
             model=load_object(model_path)
