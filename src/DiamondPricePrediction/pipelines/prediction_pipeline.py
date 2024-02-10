7168df485b17c52f459342efbd8a1fa899131435
Dennis Gwafan <denz.aviator@gmail.com>
2024-02-10 09:44:27 +0100

    file update

--- src/DiamondPricePrediction/pipelines/prediction_pipeline.py
+++ src/DiamondPricePrediction/pipelines/prediction_pipeline.py
@@ -15,6 +15,7 @@ class PredictPipeline:
             preprocessor_path=os.path.join("artifacts","preprocessor.pkl")
             model_path=os.path.join("artifacts","model.pkl")
             
+            
             preprocessor=load_object(preprocessor_path)
             model=load_object(model_path)
             
