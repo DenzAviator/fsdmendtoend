a9e6bcae4b0b1309f025355ddf49af4a53aed9fd
Dennis Gwafan <denz.aviator@gmail.com>
2024-02-15 21:24:24 +0100

    file update

--- src/DiamondPricePrediction/pipelines/prediction_pipeline.py
+++ src/DiamondPricePrediction/pipelines/prediction_pipeline.py
@@ -9,12 +9,10 @@ from src.DiamondPricePrediction.utils import load_object
 class PredictPipeline:
     def __init__(self):
         pass        
-      
     def predict(self,features):
         try:
             preprocessor_path=os.path.join("artifacts","preprocessor.pkl")
             model_path=os.path.join("artifacts","model.pkl")
-            
             
             preprocessor=load_object(preprocessor_path)
             model=load_object(model_path)
