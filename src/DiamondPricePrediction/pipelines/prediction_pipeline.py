4ffb52a5429b19de7d861fa45d11e4c8a238d277
Dennis Gwafan <denz.aviator@gmail.com>
2024-03-02 20:51:44 +0100

    file update

--- src/DiamondPricePrediction/pipelines/prediction_pipeline.py
+++ src/DiamondPricePrediction/pipelines/prediction_pipeline.py
@@ -5,7 +5,6 @@ import pandas as pd
 from src.DiamondPricePrediction.exception import CustomException
 from src.DiamondPricePrediction.logger import logging
 from src.DiamondPricePrediction.utils import load_object
-
 class PredictPipeline:
     
     def __init__(self):
--- src/DiamondPricePrediction/pipelines/training_pipeline.py
+++ src/DiamondPricePrediction/pipelines/training_pipeline.py
@@ -12,7 +12,6 @@ from src.DiamondPricePrediction.logger import logging
 from src.DiamondPricePrediction.exception import CustomException
 import pandas as pd
 
-
 obj=DataIngestion()
 
 train_data_path,test_data_path=obj.initiate_data_ingestion()
@@ -23,7 +22,6 @@ train_arr,test_arr=data_transformation.initialize_data_transformation(train_data
 
 model_trainer_obj=ModelTrainer()
 model_trainer_obj.initiate_model_training(train_arr,test_arr)
-
 
 model_eval_obj = ModelEvaluation()
 model_eval_obj.initiate_model_evaluation(train_arr,test_arr)
